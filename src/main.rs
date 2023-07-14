e39c2e3311217e92ce1b27ece516c312215c5045
tastetest <tastytothesupreme@gmail.com>
2023-07-14 14:58:41 -0600

    created basic gtk app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use std::fs::File;
 use std::io::{Error, Write};
 use xdg;
+use gtk::prelude::*;
+
+fn build_gui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("Tau");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    let button = gtk::Button::with_label("click this damn button");
+    window.add(&button);
+
+    window.show_all();
+
+}
 
 fn main() {
     // TODO! shit
+
+    let application =
+        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), 
+        Default::default());
+
+    application.connect_activate(build_gui);
+
+    application.run();
 
     println!("yo this is tau");
 
--- src/run.rs
+++ src/run.rs
@@ -1,10 +1,15 @@
 use xdg;
+use gtk::prelude::*;
+use gtk::{cairo, gdk};
+use gtk::{ApplicationWindow, Button, Fixed};
+use gio::prelude::*;
+use glib::prelude::*;
 
-#[derive(Copy, Clone, Debug)]
-pub struct Entry<T, Y> {
-    Type: T,
-    Name: Y,
-}
+// #[derive(Copy, Clone, Debug)]
+// pub struct Entry<T, Y> {
+//     type: T,
+//     name: Y,
+// }
 
 pub fn RunRs() {
     // get the xdg shit here
