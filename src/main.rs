1f58b898a0cc5a9461287751ac37c8fc17112767
tastetest <tastytothesupreme@gmail.com>
2023-07-13 19:14:47 -0600

    added winit

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,9 @@ use std::fs::File;
 use std::io::{Error, Write};
 use xdg;
 
-fn main() -> Result<(), Error> {
+fn main() {
+    // TODO! shit
 
-    let xdg_dir = xdg::BaseDirectories::with_prefix("firefox").unwrap();
+    println!("yo this is tau");
 
-    let config_path = xdg_dir
-        .place_config_file("config.ini")
-        .expect("cant create your config");
-    let mut config_file = File::create(config_path)?;
-    write!(&mut config_file, "configured = 1")?;
-    let config_match = match config_file() {
-        Ok(config_file) => config_file,
-        Err(e) => return Err(e),
-
-    };
-    return config_match;
 }
