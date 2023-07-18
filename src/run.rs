15578bd0f3308fc0191f9649402eafde7fa026e5
tastetest <tastytothesupreme@gmail.com>
2023-07-17 23:23:30 -0600

    added gio functionality with desktopappinfo filename

--- src/run.rs
+++ src/run.rs
@@ -1,11 +1,21 @@
 use gtk::prelude::*;
 use gtk::{cairo, gdk};
 use gtk::{ApplicationWindow, Button, Fixed};
-use gio::AppInfo;
 use glib::prelude::*;
 
 pub fn RunRs() {
     // get gio shit here 
-    let app = gio::AppInfo::all();
-    println!("your stuff {:?}", app);
+    let applications = gio::AppInfo::all();
+    let file_name = gio::DesktopAppInfo::from_filename("firefox");
+
+    if (file_name.is_some()) {
+        println!("nice. it's firefox");
+    } else {
+        println!("i think something went wrong {:?}", file_name);
+    }
+
+    for app in applications { 
+        app.should_show();
+    }
+
 }
