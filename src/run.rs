145f4344d7af666054db79811376ee4a6102e055
tastetest <tastytothesupreme@gmail.com>
2023-07-18 22:10:50 -0600

    fixed scale/kind of

--- src/run.rs
+++ src/run.rs
@@ -5,8 +5,8 @@ use gtk::{Application, ApplicationWindow, SearchEntry, Button, Fixed};
 pub fn draw_ui(app: &Application) {
     let draw_window = ApplicationWindow::new(app);
     let search_thingy = SearchEntry::new();
-    search_thingy.set_margin(20);
-
+    search_thingy.set_margin(300);
+    
     draw_window.add(&search_thingy);
     draw_window.show_all();
     
