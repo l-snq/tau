adce42974dd250d84e93e25b79c9801610369eee
tastetest <tastytothesupreme@gmail.com>
2023-07-20 23:34:10 -0600

    fuckin around fr

--- src/run.rs
+++ src/run.rs
@@ -13,18 +13,22 @@ pub fn draw_ui(app: &Application) {
     search_thingy.set_margin(400);
     let list_something = gtk::ListBoxRow::new();
 
-    let mut text_view = gtk::Label::new(None);
+    let row = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+    let text_view = gtk::Label::new(None);
+    let other_text_view = gtk::Label::new(None);
+    other_text_view.set_markup("Something else here");
+    row.pack_start(&text_view, false, false, 40);
+    row.pack_start(&other_text_view, false, false, 40);
 
     for item in items {
         item_to_string = item.display_name().to_string();
-
-        text_view.set_markup(&item_to_string)
+        text_view.set_markup(&item_to_string);
     }
 
     // i have to use a list box otherwise 
     // it won't let me render multiple widgets
-    hbox.add(&search_thingy);
-    hbox.add(&text_view);
+    // hbox.add(&search_thingy);
+    hbox.add(&row);
 
     draw_window.add(&hbox);
     draw_window.show_all();
@@ -32,6 +36,6 @@ pub fn draw_ui(app: &Application) {
 
 pub fn run_rs() {
     // get gio shit here 
-    let applications = gio::AppInfo::all();
+    let _applications = gio::AppInfo::all();
 
 }
