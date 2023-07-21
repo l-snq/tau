8cb9b660af1c2775f74cb232c4b6f7a7515aab9a
tastetest <tastytothesupreme@gmail.com>
2023-07-21 00:00:31 -0600

    removed pointless code. I dont need to add rows to something that already provides rows as children

--- src/run.rs
+++ src/run.rs
@@ -11,24 +11,19 @@ pub fn draw_ui(app: &Application) {
     let draw_window = ApplicationWindow::new(app);
     let search_thingy = SearchEntry::new();
     search_thingy.set_margin(400);
-    let list_something = gtk::ListBoxRow::new();
 
-    let row = gtk::Box::new(gtk::Orientation::Horizontal, 5);
     let text_view = gtk::Label::new(None);
-    let other_text_view = gtk::Label::new(None);
-    other_text_view.set_markup("Something else here");
-    row.pack_start(&text_view, false, false, 40);
-    row.pack_start(&other_text_view, false, false, 40);
 
     for item in items {
         item_to_string = item.display_name().to_string();
         text_view.set_markup(&item_to_string);
+        hbox.add(&text_view)
     }
 
     // i have to use a list box otherwise 
     // it won't let me render multiple widgets
     // hbox.add(&search_thingy);
-    hbox.add(&row);
+    hbox.add(&text_view);
 
     draw_window.add(&hbox);
     draw_window.show_all();
