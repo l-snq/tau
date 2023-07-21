8d7495830bdbd9e3be220a0fc5830e674bbf4e86
tastetest <tastytothesupreme@gmail.com>
2023-07-20 22:26:58 -0600

    rendered the name of an app i have

--- src/run.rs
+++ src/run.rs
@@ -4,39 +4,34 @@ use gtk::{Application, ApplicationWindow, SearchEntry};
 
 pub fn draw_ui(app: &Application) {
 
+    let items = gio::AppInfo::all();
+    let mut item_to_string: String;
+
     let hbox = gtk::ListBox::new();
-    let data: Vec<u8>;
     let draw_window = ApplicationWindow::new(app);
     let search_thingy = SearchEntry::new();
     search_thingy.set_margin(400);
     let list_something = gtk::ListBoxRow::new();
 
-    hbox.add(&search_thingy);
+    let mut text_view = gtk::Label::new(None);
+
+    for item in items {
+        item_to_string = item.display_name().to_string();
+
+        text_view.set_markup(&item_to_string)
+    }
+
     // i have to use a list box otherwise 
     // it won't let me render multiple widgets
-    hbox.add(&list_something);
+    hbox.add(&search_thingy);
+    hbox.add(&text_view);
 
-    //draw_window.add(&search_thingy);
     draw_window.add(&hbox);
-
     draw_window.show_all();
 }
 
 pub fn run_rs() {
     // get gio shit here 
     let applications = gio::AppInfo::all();
-    let file_name = gio::DesktopAppInfo::from_filename("firefox");
-
-    if file_name.is_some() {
-        println!("nice. it's firefox");
-    } else {
-        println!("i think something went wrong {:?}", file_name);
-    }
-
-    for app in applications { 
-        app.should_show();
-        println!("{}", app.display_name().to_string()); 
-        // this is how you fetch all app display names
-    }
 
 }
