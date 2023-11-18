10cdafc691ecf7cf2081f83f70ca2615e1e30fda
ttj <tastytothesupreme@gmail.coom>
2023-11-17 21:57:16 -0700

    changed width

--- src/run.rs
+++ src/run.rs
@@ -19,7 +19,7 @@ use std::process::Command; // FOR FUTURE USE
 pub fn draw_ui(application: &Application) {
    
    let draw_window = ApplicationWindow::builder()
-        .default_width(300)
+        .default_width(700)
         .default_height(300)
         .application(application)
         .title("Tau")
@@ -31,8 +31,58 @@ pub fn draw_ui(application: &Application) {
    LayerShell::set_keyboard_mode(&draw_window, KeyboardMode::OnDemand);
    LayerShell::auto_exclusive_zone_enable(&draw_window);
 
-
    let hbox = gtk::ListBox::new();
+   if hbox.has_focus() {
+      println!("hbox has focus")
+   };
+
+   let scrolled_window = gtk::ScrolledWindow::builder()
+        .name("scrollable window")
+        .hscrollbar_policy(gtk::PolicyType::Never)
+        .build();
+
+   let text = gtk::Label::new(None);     
+
+   let mut captured_app = AppField::new();
+
+   let mut hash = HashMap::new();
+
+   let apps = gio::AppInfo::all(); 
+
+   for app in apps {
+
+       let icon_box = gtk::Box::new(gtk::Orientation::Horizontal, 20);
+       icon_box.grab_focus();
+       let app_name = app.display_name().to_string();
+       let image_icon_setup = Image::builder()
+            .pixel_size(50)
+            .build();
+       let title = gtk::Label::new(Some(&app_name));
+
+       if let Some(gtk_icon_name) = app.icon() {
+            image_icon_setup.set_from_gicon(&gtk_icon_name);
+       }
+       hash.insert(app_name.clone(), icon_box.clone()); // THIS IS EXPENSIVE, Consider alternatives to
+       // using clone()
+
+       captured_app.update_fields(app_name.clone(), app_name.clone()); // Still using clone :|
+
+       icon_box.set_focus_on_click(true);
+       if icon_box.has_focus() {
+         println!("this has focus");
+       }
+       icon_box.prepend(&title);
+       icon_box.append(&image_icon_setup);
+       hbox.append(&icon_box);
+       
+   }
+
+   let search_label = gtk::Label::builder()
+      .label("Type to search for an app")
+      .vexpand(true)
+      .halign(gtk::Align::Center)
+      .valign(gtk::Align::Center)
+      .build();
 
    // THIS IS FOR THE KEY EVENTS
    let event_controller = gtk::EventControllerKey::new();
@@ -51,51 +101,6 @@ pub fn draw_ui(application: &Application) {
    });
 
    draw_window.add_controller(event_controller);
-
-   let scrolled_window = gtk::ScrolledWindow::builder()
-        .name("scrollable window")
-        .hscrollbar_policy(gtk::PolicyType::Never)
-        .build();
-
-   let text = gtk::Label::new(None);     
-
-   let mut captured_app = AppField::new();
-
-   let mut hash = HashMap::new();
-
-   let apps = gio::AppInfo::all(); 
-
-   for app in apps {
-
-       let icon_box = gtk::Box::new(gtk::Orientation::Horizontal, 20);
-
-       let app_name = app.display_name().to_string();
-
-       let image_icon_setup = Image::builder()
-            .pixel_size(50)
-            .build();
-        
-       let title = gtk::Label::new(Some(&app_name));
-
-       if let Some(gtk_icon_name) = app.icon() {
-            image_icon_setup.set_from_gicon(&gtk_icon_name);
-       }
-       hash.insert(app_name.clone(), icon_box.clone()); // THIS IS EXPENSIVE, Consider alternatives to
-       // using clone()
-
-       captured_app.update_fields(app_name.clone(), app_name.clone()); // Still using clone :|
-
-       icon_box.prepend(&title);
-       icon_box.append(&image_icon_setup);
-       hbox.append(&icon_box);
-   }
-
-   let search_label = gtk::Label::builder()
-      .label("Type to search for an app")
-      .vexpand(true)
-      .halign(gtk::Align::Center)
-      .valign(gtk::Align::Center)
-      .build();
 
    // search bar setup
    let search_bar = SearchBar::builder()
