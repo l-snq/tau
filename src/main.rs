bb2999c7f5ca13ea51281afdaf9774f5c773f386
lsnq <tastytothesupreme@gmail.com>
2024-09-22 18:33:43 -0600

    removed comments

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,7 @@ pub mod utils;
 const APP_ID: &str = "org.tau.Main";
 
 fn main() -> glib::ExitCode {
-    // TODO!
-
     let app = Application::builder().application_id(APP_ID).build();
-
-    //run::run_rs();
-
     app.connect_activate(run::draw_ui);
     app.run()
 }
